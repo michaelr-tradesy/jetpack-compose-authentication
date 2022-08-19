36fe5b6a2ab61669ebad5c900d8831204f631243
Michael Roebuck <michaelr@tradesy.com>
2022-08-19 10:20:19 -0500

    Transformed local variables to class properties

--- app/src/main/java/com/example/jetpackcomposecodelab101/MainApplication.kt
+++ app/src/main/java/com/example/jetpackcomposecodelab101/MainApplication.kt
@@ -1,23 +1,16 @@
 package com.example.jetpackcomposecodelab101
 
-import android.R.attr.bitmap
 import android.app.Application
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
-import android.util.Base64
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
 import com.launchdarkly.sdk.LDUser
 import com.launchdarkly.sdk.LDValue
 import com.launchdarkly.sdk.android.ConnectionInformation
+import com.launchdarkly.sdk.android.FeatureFlagChangeListener
+import com.launchdarkly.sdk.android.LDAllFlagsListener
 import com.launchdarkly.sdk.android.LDClient
 import com.launchdarkly.sdk.android.LDConfig
 import com.launchdarkly.sdk.android.LDFailure
 import com.launchdarkly.sdk.android.LDStatusListener
-import java.io.ByteArrayOutputStream
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class MainApplication : Application() {
@@ -25,10 +18,29 @@ class MainApplication : Application() {
     private lateinit var ldClient: LDClient
     private lateinit var ldConfig: LDConfig
     private lateinit var ldUser: LDUser
+    private lateinit var ldStatusListener: LDStatusListener
+    private lateinit var featureFlagChangeListener: FeatureFlagChangeListener
+    private lateinit var ldAllFlagsListener : LDAllFlagsListener
 
     override fun onCreate() {
         super.onCreate()
 
+        featureFlagChangeListener = FeatureFlagChangeListener {
+            println("LaunchDarkly registerFeatureFlagListener(): isBiometricEnabled Flag=[$it]")
+        }
+        ldStatusListener = object : LDStatusListener {
+            override fun onConnectionModeChanged(connectionInformation: ConnectionInformation?) {
+                println("LaunchDarkly onConnectionModeChanged(): " +
+                    "connectionMode=[${connectionInformation?.connectionMode}] " +
+                    "lastFailure=[${connectionInformation?.lastFailure}] " +
+                    "lastSuccessfulConnection=[${connectionInformation?.lastSuccessfulConnection}] " +
+                    "lastFailedConnection=[${connectionInformation?.lastFailedConnection}] ")
+            }
+
+            override fun onInternalFailure(ldFailure: LDFailure?) {
+                println("LaunchDarkly onInternalFailure(): ldFailure=[$ldFailure]")
+            }
+        }
         ldConfig = LDConfig.Builder()
             //Coach Roebuck's account...
             // Test
@@ -37,12 +49,13 @@ class MainApplication : Application() {
             // .mobileKey("mob-81151918-ec0c-4163-b56a-7957c6286528")
             .evaluationReasons(true)
             .inlineUsersInEvents(true)
-            .disableBackgroundUpdating(true)
+            .disableBackgroundUpdating(false)
             .build()
 
-        val firstName = "Polenta"
-        val lastName = "Drea"
-        val avatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaBJsvhB9uNRXbwnd5kBReTmaGKi9dciJzog&usqp=CAU"
+        val firstName = "Brent"
+        val lastName = "Abel"
+        val avatar =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaBJsvhB9uNRXbwnd5kBReTmaGKi9dciJzog&usqp=CAU"
         ldUser = LDUser.Builder("user-$firstName-$lastName")
             .secondary("secondary-user-$firstName-$lastName")
             .email("$firstName.$lastName@email.com")
@@ -63,37 +76,52 @@ class MainApplication : Application() {
         // Create a new LDClient instance with your environment-specific mobile SDK key
         ldClient = LDClient.init(this, ldConfig, ldUser, 5)
 
+        val connectionInformation = ldClient.connectionInformation
+        println("LaunchDarkly connectionMode=[${connectionInformation.connectionMode}] ")
+        println("LaunchDarkly lastFailedConnection=[${connectionInformation.lastFailedConnection}] ")
+        println("LaunchDarkly lastFailure: failureType=[${connectionInformation.lastFailure.failureType}] " +
+            "cause=[${connectionInformation.lastFailure.cause}]" +
+            "message=[${connectionInformation.lastFailure.message}]")
+
+        ldAllFlagsListener = LDAllFlagsListener {
+            println("LaunchDarkly registerAllFlagsListener(): allFlags=[$it]")
+        }
         val isInitialized = ldClient.isInitialized
         val isDisableBackgroundPolling = ldClient.isDisableBackgroundPolling
         val isOffline = ldClient.isOffline
-        ldClient.registerFeatureFlagListener("isBiometricEnabled") {
-            println("LaunchDarkly isBiometricEnabled Flag=[$it]")
-        }
-        ldClient.registerAllFlagsListener {
-            println("LaunchDarkly allFlags=[$it]")
-        }
-        ldClient.registerStatusListener(object : LDStatusListener {
-            override fun onConnectionModeChanged(connectionInformation: ConnectionInformation?) {
-                println("LaunchDarkly onConnectionModeChanged() connectionInformation=[$connectionInformation]")
-            }
 
-            override fun onInternalFailure(ldFailure: LDFailure?) {
-                println("LaunchDarkly onInternalFailure() ldFailure=[$ldFailure]")
-            }
-        })
+        ldClient.registerFeatureFlagListener("isBiometricEnabled", featureFlagChangeListener)
+        ldClient.registerAllFlagsListener(ldAllFlagsListener)
+        ldClient.registerStatusListener(ldStatusListener)
+
         println("LaunchDarkly isInitialized=[$isInitialized]")
         println("LaunchDarkly isDisableBackgroundPolling=[$isDisableBackgroundPolling]")
         println("LaunchDarkly isOffline=[$isOffline]")
+
         ldClient.track("Tracked Event")
         ldClient.trackData("New Data", LDValue.buildArray().add("$firstName $lastName").build())
-        ldClient.trackMetric("New Metric", LDValue.buildArray().add("$firstName $lastName").build(), 1.0)
+        ldClient.trackMetric("New Metric",
+            LDValue.buildArray().add("$firstName $lastName").build(),
+            1.0)
+        val one = ldClient.boolVariationDetail("isAmerican", Locale.getDefault() == Locale.US)
+        val two = ldClient.boolVariationDetail("isBiometricEnabled", false)
+        val three = ldClient.boolVariation("isBiometricEnabled", false)
+
+        println("LaunchDarkly isAmerican detail=[${one.value}] isDefaultValue=[${one.isDefaultValue}] reason=[${one.reason}]")
+        println("LaunchDarkly isBiometricEnabled detail=[${two.value}] isDefaultValue=[${two.isDefaultValue}] reason=[${two.reason}]")
+        println("LaunchDarkly isBiometricEnabled variation=[${three}]")
     }
 
-    val isBiometricEnabled: Boolean get() {
-         return ldClient.boolVariation("isBiometricEnabled", false)
-    }
+    val isBiometricEnabled: Boolean
+        get() {
+            return ldClient.boolVariation("isBiometricEnabled", false)
+        }
 
     override fun onTerminate() {
+        ldClient.unregisterFeatureFlagListener("isBiometricEnabled", featureFlagChangeListener)
+        ldClient.unregisterAllFlagsListener(ldAllFlagsListener)
+        ldClient.unregisterStatusListener(ldStatusListener)
+        ldClient.flush()
         ldClient.close()
         super.onTerminate()
     }
